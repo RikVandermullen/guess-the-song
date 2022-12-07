e1e0eba713856921ed8d503b8c54b81696155f4d
RikVandermullen <rik.vandermullen@gmail.com>
2022-12-07 19:54:15 +0100

    Added neo api url

--- apps/GuessTheSong/src/environments/environment.prod.ts
+++ apps/GuessTheSong/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://cswf-production.up.railway.app',
-  neoApiUrl: '',
+  neoApiUrl: 'https://melodic-underwear-production.up.railway.app',
 };
