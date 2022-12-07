81575ab0d95672f259e571e6bc73c414f387e609
RikVandermullen <rik.vandermullen@gmail.com>
2022-12-07 13:21:13 +0100

    cors fix? pt 6

--- apps/GuessTheSong/src/app/core/game/game.service.ts
+++ apps/GuessTheSong/src/app/core/game/game.service.ts
@@ -14,7 +14,7 @@ export class GameService {
   constructor(private http: HttpClient) { }
 
   getAllGames(): Observable<Game[]> {
-    const url = environment.apiUrl + "/api/games";
+    const url = "https://cswf-production.up.railway.app/api/games";
 	
 		return this.http.get<Game[]>(url).pipe(
         map((response: Game[]) => response),
